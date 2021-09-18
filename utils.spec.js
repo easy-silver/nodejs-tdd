ec3ba0ccf84ac2b9fb103422f4c8fa6d5e747193
Timo <developer.timo@gmail.com>
2021-09-18 12:22:43 +0900

    테스트 주도 개발 (TDD) - 모카(macha) 3 > assert를 should로 변경하여 테스트 결과 확인

--- utils.spec.js
+++ utils.spec.js
@@ -1,11 +1,11 @@
 const utils = require('./utils');
-const assert = require('assert');
+const should = require('should');
 
 //TEST SUITE
 describe('utils.js 모듈의 capitalize 함수는 ', () => {
     //TEST CASE
     it('문자열의 첫 번째 문자를 대문자로 변환한다.', () => {
         const result = utils.capitalize('hello');
-        assert.equal(result, 'Hello');
+        result.should.be.equal('Hello');
     })
 })
