bd40e9b8d315ddb407915d7219b52f58f6807f46
Timo <developer.timo@gmail.com>
2021-09-22 16:28:07 +0900

    코드 리펙토링 - 라우터 클래스 > 라우터 클래스를 이용하여 파일 분리

--- index.js
+++ index.js
@@ -2,77 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
-let users = [
-    { id: 1, name: 'alice' },
-    { id: 2, name: 'bek' },
-    { id: 3, name: 'chris' }
-];
+//index.js는 path에서 생략 가능
+const user = require('./api/user');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/**
- * 사용자 목록 조회 API
- */
-app.get('/users', (req, res) => {
-    req.query.limit = req.query.limit || 10;
-    const limit = parseInt(req.query.limit, 10);
-    if (Number.isNaN(limit)) {
-        return res.status(400).end();
-    }
-    res.json(users.slice(0, limit));
-});
-
-/**
- * 사용자 조회 API
- */
-app.get('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if(Number.isNaN(id)) return res.status(400).end();
-    const user = users.filter((user) => user.id === id )[0];
-    if(!user) return res.status(404).end();
-    res.json(user);
-});
-
-app.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) return res.status(400).end();
-
-    users = users.filter(user => user.id !== id);
-    res.status(204).end();
-});
-
-app.post('/users', (req, res) => {
-    const name = req.body.name;
-    if (!name) return res.status(400).end();
-
-    const isConflict = users.filter(user => user.name === name).length;
-    if (isConflict) return res.status(409).end();
-
-    const id = Date.now();
-    const user = {id, name};
-    users.push(user);
-    res.status(201).json(user);
-});
-
-app.put('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) return res.status(400).end();
-
-    const name = req.body.name;
-    if(!name) return res.status(400).end();
-
-    const isConflict = users.filter(user => user.name === name).length;
-    if(isConflict) return res.status(409).end();
-
-    const user = users.filter(user => user.id === id)[0];
-    if(!user) return res.status(404).end();
-
-    user.name = name;
-    res.json(user);
-});
+app.use('/users', user);
 
 app.listen(3000, () => {
     console.log('Example app listening at http://localhost:3000');
