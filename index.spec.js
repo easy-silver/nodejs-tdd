5d3559ff328f3ea9897dc7fd688ccd61622d440e
Timo <developer.timo@gmail.com>
2021-09-22 10:16:42 +0900

    테스트 주도 개발 (TDD) - 슈퍼테스트(superTest) 2 > should를 통해 결과 검증 추가

--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,18 @@
-const app = require('./index');
 const request = require('supertest');
+const should = require('should');
+const app = require('./index');
 
 //API 테스트의 경우 테스트 수트에 API 이름을 적는다.
 describe('GET /users는', () => {
-    it('should ', (done) => {
-        request(app)
-            .get('/users')
-            .end((err, res) => {
-                console.log(res.body);
-                done();
-            });
+    describe('성공 시', () => {
+        it('유저 객체를 담은 배열로 응답한다', (done) => {
+            request(app)
+                .get('/users')
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                });
+        });
     });
+
 });
