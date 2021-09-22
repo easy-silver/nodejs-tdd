43c71641ab33e0b5567683f339ba61509b6e7a42
Timo <developer.timo@gmail.com>
2021-09-22 17:41:51 +0900

    데이터베이스 - index 컨트롤러 연동 1

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -1,11 +1,15 @@
 const request = require('supertest');
 const should = require('should');
 const app = require('../../');
+const models = require('../../models');
+
 
 //API 테스트의 경우 테스트 수트에 API 이름을 적는다.
 describe('GET /users는', () => {
     describe('성공 시', () => {
-        it('유저 객체를 담은 배열로 응답한다', (done) => {
+        before(() => models.sequelize.sync({force: true}));
+
+        it.only('유저 객체를 담은 배열로 응답한다', (done) => {
             request(app)
                 .get('/users')
                 .end((err, res) => {
